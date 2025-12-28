0d3785bf272eab8e6f4857abd9a64b4dab43d9d0
SEANGLAY <devolutionary.wizard@gmail.com>
2025-12-28 09:35:12 +0700

    chore: add project dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
-<script lang="ts">
+<script setup lang="ts">
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen gap-6">
-    <div class="w-24 h-24 bg-red-500 hover:bg-red-700 rounded-lg shadow-lg transition-colors duration-300" />
-    <div class="w-24 h-24 bg-green-500 hover:bg-green-700 rounded-lg shadow-lg transition-colors duration-300" />
-    <div class="w-24 h-24 bg-blue-500 hover:bg-blue-700 rounded-lg shadow-lg transition-colors duration-300" />
+  <div class="min-h-screen flex flex-col items-center justify-center gap-4">
+    <p class="text-2xl font-sans">
+      Inter (sans): 00:42 / 03:15
+    </p>
+
+    <p class="text-2xl font-mono">
+      JetBrains Mono (mono): 00:42 / 03:15
+    </p>
   </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,20 @@
+import presetAttributify from '@unocss/preset-attributify'
+import UnocssIcons from '@unocss/preset-icons'
+import presetWebFonts from '@unocss/preset-web-fonts'
+import presetWind3 from '@unocss/preset-wind3'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
+  presets: [
+    presetWind3(),
+    presetAttributify(),
+    UnocssIcons({}),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Inter:400,500,600,700',
+        mono: 'JetBrains Mono:400,500,700',
+      },
+    }),
+  ],
 })
