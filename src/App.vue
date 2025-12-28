bb6d4abfe4f1877e847b34139801209223dd0787
SEANGLAY <devolutionary.wizard@gmail.com>
2025-12-28 09:06:04 +0700

    chore: initial project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen gap-6">
+    <div class="w-24 h-24 bg-red-500 hover:bg-red-700 rounded-lg shadow-lg transition-colors duration-300" />
+    <div class="w-24 h-24 bg-green-500 hover:bg-green-700 rounded-lg shadow-lg transition-colors duration-300" />
+    <div class="w-24 h-24 bg-blue-500 hover:bg-blue-700 rounded-lg shadow-lg transition-colors duration-300" />
+  </div>
+</template>
